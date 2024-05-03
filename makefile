b5d58a6e4473d866aba08d2c23fcbe0240930333
elliotrock <elliotrockcom@gmail.com>
2024-05-03 11:36:46 +1000

    feat:First commit

---
+++ makefile
@@ -0,0 +1,41 @@
+.PHONY: cli 
+cli:
+	docker exec -it ksqldb-cli ksql http://ksqldb-server:8088	
+
+.PHONY: play
+play:
+	chmod u+x play.sh
+	bash play.sh
+
+.PHONY: build
+build:
+	chmod u+x build.sh
+	bash build.sh
+	chmod u+x connectors.sh
+	bash connectors.sh
+
+# deprecated
+.PHONY: test
+test:
+	# docker exec ksqldb-cli ksql-test-runner -i /opt/app/test/input.json -s opt/app/src/statements.sql -o /opt/app/test/output-runners-pool.json
+	# docker exec ksqldb-cli ksql-test-runner -i /opt/app/test/input.json -s opt/app/src/statements.sql -o /opt/app/test/output-race-pool.json
+	# docker exec ksqldb-cli ksql-test-runner -i /opt/app/test/input.json -s opt/app/src/statements.sql -o /opt/app/test/output-bets-runners-pool.json	
+	
+	# docker exec ksqldb-cli ksql-test-runner -i /opt/app/test/input-runners.json -s opt/app/src/statements-race-runners.sql -o /opt/app/test/output-race-runners.json
+	# docker exec ksqldb-cli ksql-test-runner -i /opt/app/test/input.json -s opt/app/src/statements.sql -o /opt/app/test/output-race-runners-odds.json
+
+.PHONY: start
+start: 
+	docker-compose up
+
+.PHONY: stop
+stop: 
+	docker-compose stop
+
+.PHONY: kill
+kill: 
+	docker-compose kill
+
+.PHONY: prune
+prune: 
+	docker container prune
---
+++ src/statements.sql
@@ -0,0 +1,43 @@
+SET 'auto.offset.reset' = 'earliest';
+
+CREATE STREAM tote_win_bets (runner_id INT, race_id INT, amount DOUBLE)
+    WITH (kafka_topic='tote_win_bets', partitions=1, value_format = 'avro');
+
+-- Bets runner keyed by partition
+-- CREATE STREAM tote_win_bets_runner_keyed 
+--     AS SELECT tote_win_bets.runner_id as runner_id, tote_win_bets.race_id as race_id, amount 
+--     FROM tote_win_bets
+--     PARTITION BY runner_id
+--     EMIT CHANGES;     
+
+-- -- Bets race keyed by partition
+-- CREATE STREAM tote_win_bets_race_keyed
+--     AS SELECT tote_win_bets.race_id as race_id, tote_win_bets.amount as amount
+--     FROM tote_win_bets
+--     PARTITION BY race_id 
+--     EMIT CHANGES;
+
+-- -- Runner Pool MAX(race_id) as race_id
+-- CREATE TABLE tote_runners_pool 
+--     AS SELECT runner_id, SUM(amount) as runner_pool_amount 
+--     FROM tote_win_bets_runner_keyed GROUP BY runner_id
+--     EMIT CHANGES; 
+
+-- -- Race Pool
+-- CREATE TABLE tote_race_pool 
+--     AS SELECT race_id, SUM(amount) as race_pool_amount 
+--     FROM tote_win_bets_race_keyed GROUP BY race_id
+--     EMIT CHANGES; 
+
+-- -- form 3 way join odd stream
+-- CREATE STREAM tote_win_bet_race_runners_odds AS
+--     SELECT tote_race_pool.race_id as race_id,
+--         tote_runners_pool.runner_id as runner_id,
+--         tote_runners_pool.runner_pool_amount as runner_pool_amount,
+--         tote_race_pool.race_pool_amount as race_pool_amount,
+--         (tote_race_pool.race_pool_amount / tote_runners_pool.runner_pool_amount) as odds
+--         FROM tote_win_bets
+--         INNER JOIN tote_runners_pool ON tote_win_bets.runner_id = tote_runners_pool.runner_id   
+--         INNER JOIN tote_race_pool ON tote_win_bets.race_id = tote_race_pool.race_id
+--     EMIT CHANGES;  
+  
